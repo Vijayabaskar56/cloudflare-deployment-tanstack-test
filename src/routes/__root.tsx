9cfdf38ff24d014254977f6fee73772a439c6e08
vijayabaskar <vj2k02@gmail.com>
2025-09-27 23:17:24 +0530

    try 5

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,60 +1,54 @@
 import {
-  HeadContent,
-  Scripts,
-  createRootRouteWithContext,
-} from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanstackDevtools } from '@tanstack/react-devtools'
+	HeadContent,
+	Scripts,
+	createRootRouteWithContext,
+} from "@tanstack/react-router";
 
-import Header from '../components/Header'
+import Header from "../components/Header";
 
-import StoreDevtools from '../lib/demo-store-devtools'
+import appCss from "../styles.css?url";
 
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
-
-import appCss from '../styles.css?url'
-
-import type { QueryClient } from '@tanstack/react-query'
+import type { QueryClient } from "@tanstack/react-query";
 
 interface MyRouterContext {
-  queryClient: QueryClient
+	queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'TanStack Start Starter',
-      },
-    ],
-    links: [
-      {
-        rel: 'stylesheet',
-        href: appCss,
-      },
-    ],
-  }),
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "TanStack Start Starter",
+			},
+		],
+		links: [
+			{
+				rel: "stylesheet",
+				href: appCss,
+			},
+		],
+	}),
 
-  shellComponent: RootDocument,
-})
+	shellComponent: RootDocument,
+});
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        <Header />
-        {children}
-        <TanstackDevtools
+	return (
+		<html lang="en">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				<Header />
+				{children}
+				{/* <TanstackDevtools
           config={{
             position: 'bottom-left',
           }}
@@ -66,9 +60,9 @@ function RootDocument({ children }: { children: React.ReactNode }) {
             StoreDevtools,
             TanStackQueryDevtools,
           ]}
-        />
-        <Scripts />
-      </body>
-    </html>
-  )
+        /> */}
+				<Scripts />
+			</body>
+		</html>
+	);
 }
