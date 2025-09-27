63346751b0ab4e16d404d9cae2569ffaa132cc84
vijayabaskar <vj2k02@gmail.com>
2025-09-28 00:20:50 +0530

    vercel try 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,15 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
 import { nitro } from 'nitro/vite'
 const config = defineConfig({
   plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
-    tanstackStart({
-      client : {},
-      server : {},
-      vite : {},
-      prerender : {},
-      spa : {
-        enabled : true,
-        prerender : {
-          autoSubfolderIndex : true
-        }
-      },
-    }),
-     nitro(),
+    tanstackStart({}),
+    nitro(),
     viteReact(),
   ],
 })
